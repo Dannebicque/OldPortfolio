6e1339aba6a3e8cba5d9104d1fc5256bba743eb5
David Annebicque <david.annebicque@gmail.com>
2021-11-24 14:19:42 +0100

    feat: Connexion, inscription, password lost (#1)

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=EtudiantRepository::class)
+ */
+class Etudiant extends Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ src/Entity/Personnel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonnelRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonnelRepository::class)
+ */
+class Personnel extends Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
